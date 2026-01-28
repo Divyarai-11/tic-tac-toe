84cc235afb93937d5ae85b4612c7e6075f3c2a91
Divyarai-11 <divyaraivns33@gmail.com>
2026-01-28 13:52:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+const cells=document.querySelectorAll(".cell");
+const statusText=document.getElementById("status");
+const resetBtn=document.getElementById("reset");
+
+let currentPlayer = "X";
+let gameActive = true;
+let board = ["", "", "", "", "", "", "", "", ""]
+
+const winningConditions = [[0,1,2], [3,4,5], [6,7,8],
+                          [0,3,6], [1,4,7], [2,5,8],
+                          [0,4,8], [2,4,6]];
+
+cells.forEach (cell => cell.addEventListener("click", handleCellClick));
+resetBtn.addEventListener("click", resetGame);
+
+function handleCellClick() {
+    const index= this.getAttribute("data-index");
+
+    if (board[index] !== "" || ! gameActive) {
+        return;
+    }
+    board[index]= currentPlayer;
+    this.textContent= currentPlayer;
+
+    checkResult();
+}
+
+function checkResult() {
+    let win= false;
+
+    for (let condition of winningConditions) {
+        const [a, b, c] = condition;
+        if (board[a] && board[a] ===board[b] &&board[a] ===board[c]) {
+            win = true;
+            break;
+        } 
+    }
+     if (win) {
+        statusText.textContent= `Player ${currentPlayer} Wins!`;
+        gameActive= false;
+        return;
+     }
+     if (!board.includes("")) {
+        statusText.textContent= "Game Draw!";
+        gameActive = false;
+        return;
+     }
+     currentPlayer= currentPlayer === "X" ? "O" : "X";
+     statusText.textContent= `Player ${currentPlayer}'s Turn`;
+}
+function resetGame() {
+    currentPlayer= "X";
+    gameActive= true;
+    board = ["", "", "", "", "", "", "", "", ""];
+    statusText.textContent= "Player X's Turn";
+    cells.forEach(cell =>cell.textContent="");
+}
